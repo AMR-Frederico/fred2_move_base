0779fb3e9531dc2fe85af663eb8d9df9865967a9
Leticia <leticiarp2000@hotmail.com>
2023-11-19 13:14:37 -0300

    add subscribers

--- fred2_move_base/scripts/led_manager.py
+++ fred2_move_base/scripts/led_manager.py
@@ -5,24 +5,53 @@ import yaml
 import os
 
 from typing import Any, List, Optional
-from rcl_interfaces.msg import Parameter, ParameterDescriptor
-from rclpy.node import Node
+
 from rclpy.parameter import Parameter
+from rclpy.node import Node
+from rclpy.time import Duration, Time
 
+from rcl_interfaces.msg import Parameter
 from std_msgs.msg import Bool, Int16
+from geometry_msgs.msg import PoseStamped, Pose2D
 
+# Parameters file (yaml)
 led_path = '~/ros2_ws/src/fred2_move_base/config/move_base_params.yaml'
 led_group = 'led_manager'
 
+# Imminent collision detected by the ultrasonic sensors 
 collision_detected = False
 
+# Manual stop command send by the joystick 
 user_stop_command = True
 is_emergency_stop = False
 last_stop_command = False
 
+# Robot state, random initial value
 robot_state = -5 
 
+goal_pose = Pose2D()
+
+WAYPOINT_GOAL = 0  # Goals points that the robot must signal
+GHOST_GOAL = 1
+
+LED_goal_signal = False
+
+LED_ON_TIME = Duration(seconds=1) # It's also possible to specify nanoseconds
+LED_goal_reached = False   
+
+goal_reached = False
+last_goal_reached = False       # Sinalization
+
 class led_manager(Node): 
+    
+    white = 0
+    blue = 0
+    yellow = 0
+    pink = 0 
+    green = 0 
+    orange = 0 
+    red = 0 
+    black = 0 
     
     def __init__(self, 
                  node_name: str, 
@@ -44,6 +73,7 @@ class led_manager(Node):
 
         # Load parameters from YAML file
         self.load_params(led_path, led_group)
+        self.get_colors()
 
         self.collision_detection_sub = self.create_subscription(
                                     Bool,
@@ -57,12 +87,52 @@ class led_manager(Node):
                                     self.manual_abort_callback,
                                     10 )
         
-        self.robot_state = self.create_subscription(
-                                    Int16, 
+        self.robot_state_sub = self.create_subscription(
+                                    Int16,
+                                    'machine_states/main/robot_status',
                                     self.robot_state_callback, 
                                     10 )
 
+        self.current_goal_sub = self.create_subscription(
+                                    PoseStamped,
+                                    'goal_manager/goal/current',
+                                    self.current_goal_callback, 
+                                    10 )
+        
+        self.goal_reached_sub = self.create_subscription(
+                                    Bool, 
+                                    'goal_manager/goal/reached', 
+                                    self.goal_reached_callback, 
+                                    10 )
+
+    def get_colors(self):
+        # Get index colors
+        self.white = self.get_parameter('led_white').value
+        self.blue = self.get_parameter('led_blue').value
+        self.yellow = self.get_parameter('led_yellow').value
+        self.pink = self.get_parameter('led_pink').value
+        self.orange = self.get_parameter('led_orange').value 
+        self.red = self.get_parameter('led_red').value 
+        self.black = self.get_parameter('led_black').value 
+
+
+
+    def load_params(self, path, group): 
+        param_path = os.path.expanduser(path)
+
+        with open(param_path, 'r') as params_list: 
+            params = yaml.safe_load(params_list)
+        
+        # Get the params inside led_manager group
+        params = params.get(group, {})
+
+        # Declare parameters with values from the YAML file
+        for param_name, param_value in params.items():
+            self.declare_parameter(param_name, param_value)
+            self.get_logger().info(f'{param_name}: {param_value}')
     
+
+
     def collision_callback(self, msg): 
         global collision_detected
         collision_detected = msg.data
@@ -82,32 +152,55 @@ class led_manager(Node):
         last_stop_command = user_stop_command
 
 
+
     def robot_state_callback(self, msg): 
         global robot_state
         robot_state = msg.data
 
 
 
-    def load_params(self, path, group): 
-        param_path = os.path.expanduser(path)
+    def current_goal_callback(self, msg):
+        global goal_pose
+        goal_pose.x = msg.pose.position.x
+        goal_pose.y = msg.pose.position.y
+        goal_pose.theta = msg.pose.orientation.z
 
-        with open(param_path, 'r') as params_list: 
-            params = yaml.safe_load(params_list)
+
+
+    def goal_reached_callback(self, msg):
+        global LED_ON_TIME, LED_goal_reached
+        global goal_reached, last_goal_reached
+        global goal_pose
+        global WAYPOINT_GOAL, GHOST_GOAL, LED_goal_signal
+
+        goal_reached = msg.data
+
+        if (goal_reached == True) and (last_goal_reached == False): 
+            start_time = self.get_clock().now()
+            LED_goal_reached = True
         
-        # Get the params inside led_manager group
-        params = params.get(group, {})
+        last_goal_reached = goal_reached
 
-        # Declare parameters with values from the YAML file
-        for param_name, param_value in params.items():
-            self.declare_parameter(param_name, param_value)
-            self.get_logger().info(f'{param_name}: {param_value}')
+        if (self.get_clock().now() - start_time) > LED_ON_TIME: 
+            LED_goal_reached = False 
+
+        if goal_pose.theta == WAYPOINT_GOAL: 
+            LED_goal_signal = True
+        
+        if goal_pose.theta == GHOST_GOAL: 
+            LED_goal_signal = False
+
  
 def main():
+    
+    led_color = led_manager.pink
 
+
+if __name__ == '__main__':
+    
     rclpy.init()
 
     # Put the parameters initial state
-
 
     node = led_manager(
         'led_manager_node', 
@@ -116,6 +209,8 @@ def main():
         enable_rosout=False, 
     )
 
+    main()
+
     try: 
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -123,6 +218,3 @@ def main():
     finally:
         node.destroy_node()
         rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
