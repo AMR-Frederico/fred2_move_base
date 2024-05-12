86b77af728948a3eceb23c1504821e34bd6bc947
Leticia <leticiarp2000@hotmail.com>
2024-05-12 17:29:44 -0300

    chore: add comments for better understanding

--- fred2_move_base/scripts/joy_esp_interface.py
+++ fred2_move_base/scripts/joy_esp_interface.py
@@ -4,7 +4,6 @@ import rclpy
 import threading
 import sys
 
-
 from typing import List, Optional
 
 from rclpy.context import Context 
@@ -19,10 +18,6 @@ from rcl_interfaces.srv import GetParameters
 from std_msgs.msg import Int16, Bool
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-
-# Parameters file (yaml)
-node_path = '/home/ubuntu/ros2_ws/src/fred2_move_base/config/move_base_params.yaml'
-node_group = 'joy_esp_interface'
 
 
 # Check for cli_args 
@@ -72,46 +67,59 @@ class JoyInterfaceNode(Node):
                          start_parameter_services=start_parameter_services, 
                          parameter_overrides=parameter_overrides)
     
-        # quality protocol -> the node must not lose any message 
-        qos_profile = QoSProfile(
-            reliability=QoSReliabilityPolicy.RELIABLE, 
-            durability= QoSDurabilityPolicy.VOLATILE,
-            history=QoSHistoryPolicy.KEEP_LAST, 
-            depth=10, 
-            liveliness=QoSLivelinessPolicy.AUTOMATIC
+        self.quality_protocol()
+        self.setup_publishers()
+        self.setup_subscribers()
+
+        # load params from the config file 
+        self.load_params()
+        self.get_params()
+
+        self.add_on_set_parameters_callback(self.parameters_callback)
+
+        
+        self.last_joy_command_time = self.get_clock().now()
+    
+
+    def quality_protocol(self):
+
+        self.qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,  # Set the reliability policy to RELIABLE, ensuring reliable message delivery
+            durability= QoSDurabilityPolicy.VOLATILE,   # Set the durability policy to VOLATILE, indicating messages are not stored persistently
+            history=QoSHistoryPolicy.KEEP_LAST,         # Set the history policy to KEEP_LAST, storing a limited number of past messages
+            depth=10,                                   # Set the depth of the history buffer to 10, specifying the number of stored past messages
+            liveliness=QoSLivelinessPolicy.AUTOMATIC    # Set the liveliness policy to AUTOMATIC, allowing automatic management of liveliness
             
         )
 
+    def setup_subscribers(self): 
+
+        # -------- Get joytstick commands from firmware
         self.create_subscription(Joy, 
                                  '/joy/controller/ps4', 
                                  self.joy_callback, 
                                  10)
         
 
-        
+        # -------- Get robot states 
         self.create_subscription(Int16, 
-                            '/machine_states/robot_state', 
-                            self.manualMode_callback, 
-                            qos_profile)
+                                 '/machine_states/robot_state', 
+                                 self.manualMode_callback, 
+                                 self.qos_profile)
 
 
+    def setup_publishers(self): 
 
-        self.vel_pub = self.create_publisher(Twist, '/cmd_vel', qos_profile)      
+        # --------- Velocity command 
+        self.vel_pub = self.create_publisher(Twist, '/cmd_vel', self.qos_profile)      
 
-        self.resetOdom_pub = self.create_publisher(Bool, '/odom/reset', qos_profile)
+        # --------- Request for reset the odometry 
+        self.resetOdom_pub = self.create_publisher(Bool, '/odom/reset', self.qos_profile)
 
-        self.switchMode_pub = self.create_publisher(Bool, '/joy/machine_states/switch_mode', qos_profile) 
-
-
-        # load params from the config file 
-        self.load_params()
-        self.get_params()
+        # --------- Swicht between Manuel and Autonomous mode  
+        self.switchMode_pub = self.create_publisher(Bool, '/joy/machine_states/switch_mode', self.qos_profile) 
 
 
-        self.add_on_set_parameters_callback(self.parameters_callback)
-
-        
-        self.last_joy_command_time = self.get_clock().now()
 
 
     def load_params(self):
@@ -119,18 +127,52 @@ class JoyInterfaceNode(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('drift_analog_tolerance', None, ParameterDescriptor(description='Tolerance for analog drift in joystick', type=ParameterType.PARAMETER_INTEGER)),
-                ('max_value_controller', None, ParameterDescriptor(description='Maximum value for joystick controller', type=ParameterType.PARAMETER_INTEGER)),
-                ('max_vel_joy_angular', None, ParameterDescriptor(description='Maximum angular velocity from joystick', type=ParameterType.PARAMETER_DOUBLE)),
-                ('max_vel_joy_linear', None, ParameterDescriptor(description='Maximum linear velocity from joystick', type=ParameterType.PARAMETER_DOUBLE)),
-                ('debug', None, ParameterDescriptor(description='Enable debug prints for troubleshooting', type=ParameterType.PARAMETER_BOOL)),
-                ('unit_test', None, ParameterDescriptor(description='Enable unit testing mode', type=ParameterType.PARAMETER_BOOL))
+                ('drift_analog_tolerance', None, 
+                    ParameterDescriptor(
+                        description='Tolerance for analog drift in joystick', 
+                        type=ParameterType.PARAMETER_INTEGER)),
+
+                ('max_value_controller', None, 
+                    ParameterDescriptor(
+                        description='Maximum value for joystick controller', 
+                        type=ParameterType.PARAMETER_INTEGER)),
+
+                ('max_vel_joy_angular', None, 
+                    ParameterDescriptor(
+                        description='Maximum angular velocity from joystick', 
+                        type=ParameterType.PARAMETER_DOUBLE)),
+
+                ('max_vel_joy_linear', None, 
+                    ParameterDescriptor(
+                        description='Maximum linear velocity from joystick', 
+                        type=ParameterType.PARAMETER_DOUBLE)),
+
+                ('debug', None, 
+                    ParameterDescriptor(
+                        description='Enable debug prints for troubleshooting',
+                        type=ParameterType.PARAMETER_BOOL)),
+                        
+                ('unit_test', None, 
+                    ParameterDescriptor(
+                        description='Enable unit testing mode', 
+                        type=ParameterType.PARAMETER_BOOL)),
+                
+                ('frequency', None, 
+                    ParameterDescriptor(
+                        description='Node frequency', 
+                        type=ParameterType.PARAMETER_INTEGER)),
+
+                ('joy_cmd_timeout', None, 
+                    ParameterDescriptor(
+                        description='Timeout duration (in nanoseconds) to reset the joy_cmd status if no message is received within this time.', 
+                        type=ParameterType.PARAMETER_INTEGER)),
             ]
         )
 
         self.get_logger().info('All parameters successfully declared')
 
 
+    # updates the parameters when they are changed by the command line
     def parameters_callback(self, params):
         
         for param in params:
@@ -159,18 +201,30 @@ class JoyInterfaceNode(Node):
 
         if param.name == 'unit_test': 
             self.UNIT_TEST = param.value 
+        
+
+        if param.name == 'frequency': 
+            self.FREQUENCY = param.value 
+
+
+        if param.name == 'joy_cmd_timeout': 
+            self.JOY_TIMEOUT = param.value
 
 
         return SetParametersResult(successful=True)
     
 
-
+    # get the params value 
     def get_params(self):
 
         self.MAX_VEL_JOY_LINEAR = self.get_parameter('max_vel_joy_linear').value
         self.MAX_VEL_JOY_ANGULAR = self.get_parameter('max_vel_joy_angular').value
+
         self.MAX_VALUE_CONTROLLER = self.get_parameter('max_value_controller').value
         self.DRIFT_ANALOG_TOLERANCE = self.get_parameter('drift_analog_tolerance').value
+        self.JOY_TIMEOUT = self.get_parameter('joy_cmd_timeout').value 
+
+        self.FREQUENCY = self.get_parameter('frequency').value
         self.UNIT_TEST = self.get_parameter('unit_test').value
         self.DEBUG = self.get_parameter('debug').value
 
@@ -221,37 +275,34 @@ class JoyInterfaceNode(Node):
 
 
     def joy_callback(self, joy_msg): 
-
-        # analog 
-
+ 
+        # Process linear velocity command from joystick analog input
         if abs(joy_msg.axes[0]) > self.DRIFT_ANALOG_TOLERANCE: 
 
             self.joy_vel_linear = joy_msg.axes[0]
 
         else: 
 
-            self.joy_vel_linear = 0
+            self.joy_vel_linear = 0                     # If joystick analog input is below tolerance, set linear velocity to 0
 
 
-
-        # analog 
-
+        # Process angular velocity command from joystick analog input
         if abs(joy_msg.axes[2]) > self.DRIFT_ANALOG_TOLERANCE: 
 
-            self.joy_vel_angular = joy_msg.axes[2]
+            self.joy_vel_angular = joy_msg.axes[2]      
 
         else: 
 
-            self.joy_vel_angular = 0 
+            self.joy_vel_angular = 0                    # If joystick analog input is below tolerance, set angular velocity to 0
 
 
 
-        # buttons 
-        reset_button = joy_msg.buttons[1]
-        switch_button = joy_msg.buttons[2]
+        # Process button commands
+        reset_button = joy_msg.buttons[1]               # CIRCLE -> Button for resetting odometry
+        switch_button = joy_msg.buttons[2]              # TRIANGLE -> Button for switching mode
     
 
-
+         # Check if reset button is pressed
         if reset_button > self.last_reset: 
 
             self.get_logger().info('Reset odometry')
@@ -259,11 +310,11 @@ class JoyInterfaceNode(Node):
 
         else: 
 
-            self.reset_robot_odom(False)
+            self.reset_robot_odom(False)                # If reset button is not pressed, do not reset odom   
       
         
 
-
+        # Check if switch button is pressed
         if switch_button > self.last_switch: 
             
             self.get_logger().info('Switch mode')
@@ -271,18 +322,19 @@ class JoyInterfaceNode(Node):
 
         else: 
 
-            self.switch_robot_mode(False)
+            self.switch_robot_mode(False)               # If switch button is not pressed, do not switch mode
 
 
-        
+         # Update time of last joystick command
         self.last_joy_command_time = self.get_clock().now()
         
+         # Update last switch and reset button states for next iteration
         self.last_switch = switch_button
         self.last_reset = reset_button
 
 
 
-
+    # publish the switch mode status 
     def switch_robot_mode(self, status): 
 
         
@@ -291,7 +343,7 @@ class JoyInterfaceNode(Node):
         self.switchMode_pub.publish(change_mode)
 
 
-
+    # publish the reset mode status 
     def reset_robot_odom(self, status): 
 
 
@@ -301,48 +353,39 @@ class JoyInterfaceNode(Node):
         self.resetOdom_pub.publish(reset_msg)
 
     
-
+    # Checks if the robot is in the manuel mode 
     def manualMode_callback(self, msg):
 
         robot_state = msg.data
-
-        if robot_state == self.ROBOT_MANUAL: 
-
-            self.manual_mode = True
+        self.manual_mode = (robot_state == self.ROBOT_MANUAL)
         
-        else: 
-
-            self.manual_mode = False
     
-
-
-
 
     def main(self): 
 
         current_time = self.get_clock().now()
 
+        # Calculate linear and angular velocities from joystick input
         vel_linear = self.joy_vel_linear * (self.MAX_VEL_JOY_LINEAR / self.MAX_VALUE_CONTROLLER)
         vel_angular = self.joy_vel_angular * (self.MAX_VEL_JOY_ANGULAR / self.MAX_VALUE_CONTROLLER)  
 
-        if (current_time - self.last_joy_command_time).nanoseconds > 1.5e8  and self.manual_mode: 
+        # Check if the joystick command timeout has been exceeded and the robot is in manual mode
+        if (current_time - self.last_joy_command_time).nanoseconds > self.JOY_TIMEOUT  and self.manual_mode: 
 
             vel_linear = 0.0
             vel_angular = 0.0
         
 
-        # cmd vel msg 
+        # Create Twist message for velocity command
         self.cmd_vel.linear.x = vel_linear
-        self.cmd_vel.angular.z = -1 * vel_angular   # correção de sentido
+        self.cmd_vel.angular.z = -1 * vel_angular   # Angular velocity correction
 
-
+        # Publish velocity command only if in manual mode
         if self.manual_mode: 
 
             self.vel_pub.publish(self.cmd_vel)
 
 
-
-        
         if debug_mode or self.DEBUG:
              
             self.get_logger().info(f'Velocity -> linear:{vel_linear} | angular:{vel_angular}\n')
@@ -365,7 +408,7 @@ if __name__ == '__main__':
     thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
     thread.start()
 
-    rate = node.create_rate(7)
+    rate = node.create_rate(node.FREQUENCY)
 
     try: 
         while rclpy.ok(): 
